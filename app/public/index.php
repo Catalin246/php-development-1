574e09c19e0fd2f31ea2fe8a92cddd85c4ca7842
Catalin246 <avorniceseicatalin1234@gmail.com>
2023-11-10 23:07:24 +0100

    Display the php,ini information.

--- app/public/index.php
+++ app/public/index.php
@@ -8,27 +8,9 @@
 </head>
 
 <body>
-    <form action="" method="POST">
-        <label for="name">Name:</label><br>
-        <input type="text" id="name" name="name" value="Catalin"><br>
-        <label for="birthdate">Birthdate:</label><br>
-        <input type="date" id="birthdate" name="birthdate" value="2002-01-28"><br><br>
-        <input type="submit" value="Submit">
-    </form>
-
     <?php
 
-    if (isset($_POST['name']) && isset($_POST['birthdate'])) {
-        $name = $_POST['name'];
-        $birthdate = date('Y/m/d', strtotime($_POST['birthdate']));
-
-        echo "<h1>User Information</h1>";
-        echo "<p>Name: $name</p>";
-        echo "<p>Birthdate: $birthdate</p>";
-    } else {
-        echo "<h1>Error</h1>";
-        echo "<p>Both name and birthdate parameters are required.</p>";
-    }
+    phpinfo();
 
     ?>
 </body>
