a36b6e3647be94b157ae29a11b4b366b5fe7c294
Catalin246 <avorniceseicatalin1234@gmail.com>
2023-11-10 22:51:00 +0100

    Rebase the main branch.

--- app/public/index.php
+++ app/public/index.php
@@ -1,26 +1,3 @@
 <?php
-
-// Calculate age based on birthdate
-function calculateAge($birthdate) {
-    $today = new DateTime();
-    $birthdate = new DateTime($birthdate);
-    $age = $today->diff($birthdate);
-    return $age->y;
-}
-
-// Retrive name and birth date from URL
-// URL structure: localhost?name=Catalin&birthdate=2002-01-28
-
-if (isset($_GET['name']) && isset($_GET['birthdate'])) {
-    $name = htmlspecialchars($_GET['name']);
-    $birthdate = htmlspecialchars($_GET['birthdate']);
-
-    $age = calculateAge($birthdate);
-
-    echo "<h1>User Information</h1>";
-    echo "<p>Name: $name</p>";
-    echo "<p>Age: $age years</p>";
-} else {
-    echo "<h1>Error</h1>";
-    echo "<p>Both name and birthdate parameters are required.</p>";
-}
+echo "Requested URL: " . $_SERVER['REQUEST_URI'];
+phpinfo();
