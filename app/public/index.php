fb2afd0e59c9616918a3ffff1930fbe72205bbc9
Catalin246 <avorniceseicatalin1234@gmail.com>
2023-11-11 12:06:56 +0100

    Assignment extra.

--- app/public/index.php
+++ app/public/index.php
@@ -1,81 +1,76 @@
+<?php require("script.php") ?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
+    <!-- Setting the pages character encoding -->
     <meta charset="UTF-8">
+
+    <!-- The meta viewport will scale my content to any device width -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Development</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+
+    <!-- Link to my stylesheet -->
+    <link rel="stylesheet" href="styles.css">
+    <?php
+    if ($username_error != null) {
+        ?>
+        <style>
+            .username-error {
+                display: block
+            }
+        </style>
+        <?php
+    }
+    if ($password_error != null) {
+        ?>
+        <style>
+            .password-error {
+                display: block
+            }
+        </style>
+        <?php
+    }
+    if ($success != null) {
+        ?>
+        <style>
+            .success {
+                display: block
+            }
+        </style>
+        <?php
+    }
+    ?>
+
+    <title>Displaying errors with PHP</title>
 </head>
 
 <body>
-    <div class="container">
-        <h1 class="pt-5">Assignment Form</h1>
-        <form method="POST">
-            <label>Full name:</label><br>
-            <input type="text" name="name" placeholder="Full name"><br><br>
-            <label>Postal code:</label><br>
-            <input type="text" name="postalcode" placeholder="Postal code">
+    <div class="page">
 
-            <?php if (!isset($errors['postal_code'])): ?>
-                <p style="color: red;">
-                    <?= $errors['postal_code'] ?>
-                </p>
-            <?php endif; ?>
+        <!-- <h1>Displaying form errors with PHP</h1> -->
 
-            <br><br>
-            <label>Preferred language:</label><br>
-            <select name="language"><br>
-                <option value="EN">English</option>
-                <option value="NL">Dutch</option>
-                <option value="DE">German</option>
-                <option value="FR">French</option>
-            </select><br><br>
-            <label>Extra options:</label><br>
-            <input type="checkbox" name="options" value="Vegetarian"> Vegetarian<br>
-            <input type="checkbox" name="options" value="Gluten free"> Gluten free<br>
-            <input type="checkbox" name="options" value="Lactose free"> Lactose
-            free<br><br>
-            <label>Remarks:</label><br>
-            <textarea name="remarks" id="" cols="30" rows="10"></textarea><br><br>
-            <input type="submit" value="Submit">
+        <form action="" method="post" autocomplete="off">
+
+            <label>Username <span>Choose a username</span> </label>
+            <input type="text" name="username" value="<?php echo $username; ?>">
+            <p class="error username-error">
+                <?php echo $username_error; ?>
+            </p>
+
+            <label>Password <span>Choose a password</span> </label>
+            <input type="text" name="password" value="<?php echo $password; ?>">
+            <p class="error password-error">
+                <?php echo $password_error; ?>
+            </p>
+
+            <input type="submit" name="sign-up" value="Sign Up">
+            <p class="success">
+                <?php echo $success; ?>
+            </p>
         </form>
-    </div>
 
-    <div class="container">
-        <?php
 
-        $errors = [];
 
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            if (isset($_POST['name']) && isset($_POST['postalcode']) && isset($_POST['language'])) {
-                // Save the post valus in variables
-                $name = $_POST['name'];
-                $postal_code = $_POST['postalcode'];
-                $language = $_POST['language'];
-
-                // Validate if the postal code is filled in
-                if (empty($postalCode)) {
-                    $errors['postal_code'] = 'Postal code is required.';
-                } else {
-                    // Validate the format of the postal code
-                    if (!preg_match('/^\d{4} [a-zA-Z]{2}$/', $postalCode)) {
-                        $errors['postal_code'] = 'Invalid postal code format. It should be 4 numbers, a space, and 2 letters.';
-                    }
-                }
-
-                if (empty($errors)) {
-                    // Display the form data
-                    echo "<h1>Form Results</h1>";
-                    echo "<p>Name: $name</p>";
-                    echo "<p>Post Code: $postal_code</p>";
-                    echo "<p>Language: $language</p>";
-                }
-            }
-        }
-
-        ?>
     </div>
 </body>
 
