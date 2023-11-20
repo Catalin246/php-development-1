3e8088d8a6fe50c75143471f7b728aae3f14fcb5
Catalin246 <avorniceseicatalin1234@gmail.com>
2023-11-20 18:51:40 +0100

    Clean the main branch.

--- app/public/index.php
+++ app/public/index.php
@@ -1,33 +1,3 @@
 <?php
-
-session_start();
-
-if (!isset($_SESSION["authenticated"]) || $_SESSION["authenticated"] !== true) {
-    header("Location: login.php");
-    exit();
-}
-
-if (isset($_GET['logout'])) {
-    session_destroy();
-    header("Location: login.php");
-    exit();
-}
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bootstrap</title>
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-</head>
-
-<body>
-
-</body>
-
-</html>
+echo "Requested URL: " . $_SERVER['REQUEST_URI'];
+phpinfo();
