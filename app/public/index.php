10a2bad358161bf3ff937791fc9fee95726c9a2d
Catalin246 <avorniceseicatalin1234@gmail.com>
2023-11-20 19:07:45 +0100

    Create a tremplate page using bootstrap grid.

--- app/public/index.php
+++ app/public/index.php
@@ -1,19 +1,3 @@
-<?php
-
-session_start();
-
-if (!isset($_SESSION["authenticated"]) || $_SESSION["authenticated"] !== true) {
-    header("Location: login.php");
-    exit();
-}
-
-if (isset($_GET['logout'])) {
-    session_destroy();
-    header("Location: login.php");
-    exit();
-}
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -21,13 +5,50 @@ if (isset($_GET['logout'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bootstrap</title>
-
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-
+    <div class="container">
+        <div class="row">
+            <p>Nav Menu</p>
+        </div>
+        <div class="row">
+            <h1>BANNER</h1>
+        </div>
+        <div class="row">
+            <div class="col-3">
+                <p>menu</p>
+            </div>
+            <div class="col-9">
+                <h1>main content</h1>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident ipsa unde eius pariatur quaerat
+                    fugit possimus nisi, deserunt recusandae, laboriosam excepturi quidem ut consequatur cupiditate sit
+                    nesciunt voluptas magni quo, aperiam debitis reiciendis. Natus, fugit? A, voluptates ab quaerat
+                    eaque iste doloremque impedit sed. Culpa numquam tenetur sapiente debitis sint?</p>
+                <div class="row mx-auto">
+                    <div class="col-4">
+                        Feature 1
+                    </div>
+                    <div class="col-4">
+                        Feature 2
+                    </div>
+                    <div class="col-4">
+                        Feature 3
+                    </div>
+                </div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, expedita. Amet iusto eius commodi
+                    laboriosam sit aliquid aut doloribus! Adipisci illo ducimus odio. Dolorum nobis ipsa non? Nulla ipsa
+                    inventore est fugit commodi, perferendis suscipit voluptatibus aut impedit molestias illo,
+                    perspiciatis sunt beatae quia repellendus ex quas reiciendis, error deleniti.</p>
+            </div>
+        </div>
+        <div class="row">
+            <p>Footer</p>
+        </div>
+    </div>
 </body>
 
 </html>
