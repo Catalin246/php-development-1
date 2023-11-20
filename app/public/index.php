a20082be24306e353313f288b6e61114b7d4462f
Catalin246 <avorniceseicatalin1234@gmail.com>
2023-11-20 19:24:13 +0100

    Add the navbar and a banner.

--- app/public/index.php
+++ app/public/index.php
@@ -13,6 +13,23 @@
 </head>
 
 <body>
+    <nav class="navbar bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand">Navbar</a>
+            <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
+
+    <div class="container text-center">
+        <div class="row">
+            <h1>The best products</h1>
+            <h>Here you will find fresh articles, specially curated for you by our experts.</h5>
+        </div>
+    </div>
+
     <section>
         <div class="container">
             <h2>Miscellaneous</h2>
